399337a139289828b9552db408294f7ddf4fbab0
Geoffrey Tomagan <tomagan.geoffreyadam@gmail.com>
2026-07-07 03:54:49 +0800

    feat: implement application fetching and display in MyTracker component

--- client/src/pages/MyTracker.jsx
+++ client/src/pages/MyTracker.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import apiFetch from "../services/api";
+
 function MyTracker(){
-    return <h1>My Tracker Page</h1>;
+    const [applications, setApplication] = useState([]);
+    
+    useEffect(() => {
+        apiFetch("/applications").then((data) => setApplication(data.applications)).catch((error) => console.error("Failed to load applications:", error))
+    }, []);
+
+    return(
+        <div>
+            <h1>My Tracker</h1>
+            {applications.length === 0 ? (
+                <p>No applications yet - browse scholarships to get started.</p>
+            ) : (
+                <ul>
+                    {applications.map((app) => (
+                        <li key={app.id}>
+                            <h3>{app.title}</h3>
+                            <p>{app.organization}</p>
+                            <p>Status: {app.application_status}</p>
+                            <p>Description: {app.description}</p>
+                            <p>Deadline: {app.deadline}</p>
+                            <p>Requirements: {app.requirements}</p>
+                            <p>Amount: {app.amount}</p>
+                            <p>Slots: {app.slots}</p>
+                            <p>Notes: {app.notes}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
 }
 
 export default MyTracker;
